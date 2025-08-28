ad79a07dcfb4d53dcbf52fac493b95fd11b3ef75
philong-inco <philong.inco@gmail.com>
2025-08-29 00:10:24 +0700

    upload

---
+++ AspNetCoreBase/Setting/AppSetting.cs
@@ -0,0 +1,48 @@
+﻿using Confluent.Kafka;
+
+namespace Main.Setting;
+
+/// <summary>
+/// Lưu trữ thông tin cấu hình ứng dụng
+/// </summary>
+public class AppSetting
+{
+    public string DBDefaultTableSchema { get; init; }
+    public string DB1ConnectionString { get; init; }
+    public string DB2ConnectionString { get; init; }
+    public int InstanceNumber { get; init; }
+    public int TotalInstances { get; init; }
+    public ConsumerSetting[] ConsumerSettings { get; set; } 
+    public ProducerSetting[] ProducerSettings { get; set; }
+
+    public static AppSetting MapValue(IConfiguration configuration)
+    {
+        // Service information map
+        var serviceInformation = configuration.GetSection("ServiceInformation");
+        var instanceNumber = serviceInformation.GetValue<int>("InstanceNumber");
+        var totalInstances = serviceInformation.GetValue<int>("TotalInstances");
+
+        // Consumer settings map
+        var consumerSettings = configuration.GetSection("ConsumerConfigs").GetChildren();
+        foreach (var consumerSetting in consumerSettings)
+        {
+            
+        }
+
+        // Producer settings map
+        var producerSettings = configuration.GetSection("ProducerConfigs").GetChildren();
+        foreach (var producerSetting in producerSettings)
+        {
+
+        }
+
+        var appSetting = new AppSetting();
+        return appSetting;
+    }
+
+    public ConsumerSetting GetConsumerSetting(string id)
+    => ConsumerSettings.FirstOrDefault(consumerConfig => consumerConfig.Id.Equals(id));
+
+    public ProducerSetting GetProducerSetting(string id)
+    => ProducerSettings.FirstOrDefault(producerConfig => producerConfig.Id.Equals(id));
+}
---
+++ AspNetCoreBase/Setting/KafkaConfigWrapper.cs
@@ -0,0 +1,14 @@
+﻿using Confluent.Kafka;
+
+namespace Main.Setting;
+
+public class ConsumerSetting : ConsumerConfig
+{
+    public string? Id { get; init; }
+
+}
+
+public class ProducerSetting : ProducerConfig
+{
+    public string? Id { get; init; }
+}
